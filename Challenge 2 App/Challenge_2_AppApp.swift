726abd36576a377c40966cfb7ed79b56ae4ec2fa
Paviesh <sundaram.paviesh@sji.edu.sg>
2026-08-08 11:59:23 +0800

    Initial Commit

---
+++ Challenge 2 App/Challenge_2_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_2_AppApp.swift
+//  Challenge 2 App
+//
+//  Created by T Krobot on 8/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_2_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge 2 App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Challenge 2 App
+//
+//  Created by T Krobot on 8/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
