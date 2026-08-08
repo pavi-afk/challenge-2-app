de2d9c2ff42ecc4e0172a3c510fdf0851c3678a9
Val <chia_kia_min_val_2028@mgs.sch.edu.sg>
2026-08-08 12:12:35 +0800

    counter

--- Challenge 2 App/ContentView.swift
+++ Challenge 2 App/ContentView.swift
@@ -7,15 +7,32 @@
 
 import SwiftUI
 
+//ContentView.Swift
 struct ContentView: View {
+    
+    @State private var counter = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("\(counter) score")
+            Button {
+                counter += 1
+            } label: {
+                Text("+1 score")
+                    .padding()
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .clipShape(.rect(cornerRadius: 10))
+            }
+            .shadow(color: .white.opacity(0.3),
+                    radius: 10, x: 0.0, y: 0.0)
+                        
+            
+            Button("-1 score") {
+                counter -= 1
+            }
+            .buttonStyle(.bordered)
         }
-        .padding()
     }
 }
 
